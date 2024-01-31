b040609b53a718ed158e05a095127e6ee2b3a3e7
Akash-YS05 <akash.pan05@gmail.com>
2024-02-01 00:25:37 +0530

    update

---
+++ Javascript DOM/2. script (DOM Events).js
@@ -0,0 +1,9 @@
+const btn2 = document.querySelector('#v2')
+btn2.onclick = () => {
+    console.log('Yes it works!!');
+}
+
+const btn3 = document.querySelector('#v3')
+btn3.addEventListener('click', () => {
+    console.log('this method works too!!');
+})
--- Javascript DOM/app.js
+++ Javascript DOM/app.js
@@ -13,3 +13,22 @@ const t2 = document.querySelector('#banner')   //id
 const t3 = document.querySelector('.square')   //class
 const t4 = document.querySelector('img:nth-of-type(2)')
 const t5 = document.querySelector('a[title="Java"]')   //tag[attribute]
+const t6 = document.querySelector('h1').textContent('Heading changed')
+const t7 = document.querySelector('img').getAttribute('alt', 'Alt changed')
+const t8 = document.createElement('img')
+t8.src = "" //add source here
+document.body.appendChild(t8) //body is the location where we wanna append
+t8.classList.add('') //add any class
+const t9 = document.createElement('h2')
+h2.innerText = 'hello there!!!'
+document.body.appendChild(t9)
+const t10 = document.createElement('p')
+t10.append('this is the append method', 'another text block!!')
+//append and appendChild adds the stuff at the end
+const t11 = document.createElement('p')
+t11.prepend('prepend method adds at the beginning')
+//now adding stuff adjacently :
+const t12 = document.createElement('p')
+t12.append('adjacent method!!')
+const t13 = document.querySelector('h1')
+t13.insertAdjacentElement('afterend', t12)
