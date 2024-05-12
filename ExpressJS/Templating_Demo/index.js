63ea7f33b1924c7af45b0a636458049d48dca90c
Akash-YS05 <akash.pan05@gmail.com>
2024-05-12 20:44:53 +0530

    update

--- ExpressJS/Templating_Demo/index.js
+++ ExpressJS/Templating_Demo/index.js
@@ -10,20 +10,20 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))    //to be able to start the app from a different directory
 
 app.get('/', (req, res) => {
-   res.render('home.ejs')      // also can be => res.render('home') if the view engine is set as ejs
-})
+   res.render('home.ejs', { name: 'home' })      // also can be => res.render('home') if the view engine is set as ejs
+})                                               //name: home is to apply bootstrap code to all paths and thus defining the name at the root itself
 
 
 app.get('/random', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1
-    res.render('random', { rand: num })      //stored the int value in a variable "rand"
+    res.render('random', { rand: num, name: 'random' })      //stored the int value in a variable "rand"
 })
 
 app.get('/users', (req, res) => {
     const users = [
         'akash', 'aman', 'pikachu', 'orange'
     ]
-    res.render('users', { users })
+    res.render('users', { users, name: 'users' })
 })
 
 app.get('/r/:subreddit', (req, res) => {
@@ -32,7 +32,7 @@ app.get('/r/:subreddit', (req, res) => {
     if (data) {
         res.render('subreddit', {...data})
     } else {
-        res.render('notfound', {subreddit})
+        res.render('notfound', {subreddit, name: 'not found'})
     }
     
 })
