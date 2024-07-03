b495c9d41776e0b9854ae9e6aaf15fc1a4e6b53d
Akash-YS05 <akash.pan05@gmail.com>
2024-07-03 22:18:31 +0530

    update

--- ExpressJS/Error Handling/index.js
+++ ExpressJS/Error Handling/index.js
@@ -25,7 +25,14 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
 
-app.get('/products', async (req, res) => {
+function wrapAsync(fn) {
+    return function(req, res, next) {
+        fn(req, res, next).catch(e => next(e))
+    }
+}
+
+app.get('/products', wrapAsync(async (req, res, next) => {
+   
     const {category} = req.query
     if (category) {
         const products = await Product.find({category})
@@ -35,47 +42,60 @@ app.get('/products', async (req, res) => {
         res.render('products/index', { products, category: 'All' })
     }
    
-})
+    
+   
+}))
 
 app.get('/products/new', (req, res) => {
-    // res.render('products/new', {categories})
-    throw new AppError('Invalid invalid', 401)
+    res.render('products/new', {categories})
+    // throw new AppError('Invalid invalid', 401)
 })
 
-app.post('/products',async (req, res) => {
-    //console.log(req.body)
-    const newProduct = new Product(req.body)
-    await newProduct.save()
-    res.redirect(`/products/${newProduct._id}`)
-})
+app.post('/products', wrapAsync(async (req, res, next) => {
+        const newProduct = new Product(req.body)
+        await newProduct.save()
+        res.redirect(`/products/${newProduct._id}`)
+   
+}))
 
-app.get('/products/:id', async (req, res, next) => {
+
+
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
+   
     const { id } = req.params
     const product = await Product.findById(id)
-    // console.log(product)
     if (!product) {
-        next(new AppError('Product not found', 404))
+        throw new AppError('Product not found', 404)     
     }
     res.render('products/details', { product })
-})
 
-app.get('/products/:id/edit', async (req, res) => {
+}))
+
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
     const { id } = req.params
     const product = await Product.findById(id)
+    if (!product) {
+        return next(new AppError('Product not found', 404))    
+    }
     res.render('products/edit', { product, categories })
-})
+}))
 
-app.put('/products/:id', async (req, res) =>{
+app.put('/products/:id', wrapAsync(async (req, res, next) =>{
+    
     const { id } = req.params
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
     res.redirect(`/products/${product._id}`)
-})
+    
+    
+}))
 
-app.delete('/products/:id', async (req, res) => {
-    const { id } = req.params
-    const product = await Product.findByIdAndDelete(id)
-    res.redirect('/products')
-})
+app.delete('/products/:id', wrapAsync(async (req, res, next) => {
+  
+        const { id } = req.params
+        const product = await Product.findByIdAndDelete(id)
+        res.redirect('/products')
+    
+}))
 
 app.use((err, req, res, next) => {
     const {status = 500, message = 'ERROR'} = err
---
+++ Mongo Relationships/models/user.js
@@ -0,0 +1,54 @@
+const { strikethrough } = require('colors')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/relationDB')
+    .then(() => {
+        console.log('Connection open!')
+    })
+    .catch(err => {
+        console.log('oh no')
+        console.log(err)
+    })
+
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
+        {
+            _id: {_id: false},   //turns off the id assigning feature to each address
+            street: String,
+            city: String,
+            state: String,
+            country: String
+        }
+    ]
+})
+
+const User = mongoose.model('User', userSchema) 
+const makeUser = async () => {
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter'
+    })
+    u.addresses.push({
+        street: '123 sesame st.',
+        city: 'new york',
+        state: 'ny',
+        country: 'USA'
+    })
+    const res = await u.save()
+    console.log(res)
+}
+
+const addAddress = async(id) => {
+    const user = await User.findById(id)
+    user.addresses.push({
+        street: '3rd st.',
+        city: 'new york',
+        state: 'ny',
+        country: 'USA'
+    })
+    const res = await user.save()
+    console.log(res)
+}
+addAddress('66855831d8a825997f8e8073')
+// makeUser()
